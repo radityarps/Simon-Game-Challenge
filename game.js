a77ed1268d128ba03f94578322817a29a04ffa99
radityarps <radityarafifps@gmail.com>
2023-07-28 11:19:02 +0700

    Added button click

--- game.js
+++ game.js
@@ -1,8 +1,8 @@
 // Game Path
+const userClickedPattern = [];
 const gamePattern = [];
 //Random Pick Color
 var buttonColors = ["red", "blue", "green", "yellow"];
-
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
@@ -13,6 +13,17 @@ function nextSequence() {
   }, 250);
   whatSound(randomChosenColor);
 }
+
+//Clicked Button
+$(".btn").on("click", function () {
+  var userChosenColor = this.id;
+  userClickedPattern.push(userChosenColor);
+  $("#" + userChosenColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + userChosenColor).removeClass("pressed");
+  }, 200);
+  whatSound(userChosenColor);
+});
 
 //Sound List
 function whatSound(whatColor) {
