ff040bc95ac4eb2ca77f7fe287ebb74e41fc80e8
radityarps <radityarafifps@gmail.com>
2023-07-28 11:03:00 +0700

    Added sound to each color

--- game.js
+++ game.js
@@ -1,16 +1,43 @@
 // Game Path
 const gamePattern = [];
 //Random Pick Color
-var buttonColours = ["red", "blue", "green", "yellow"];
+var buttonColors = ["red", "blue", "green", "yellow"];
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColour = buttonColours[randomNumber];
-  gamePattern.push(randomChosenColour);
-  $("#" + randomChosenColour).addClass("pressed");
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor).addClass("pressed");
   setTimeout(function () {
-    $("#" + randomChosenColour).removeClass("pressed");
+    $("#" + randomChosenColor).removeClass("pressed");
   }, 250);
+  whatSound(randomChosenColor);
 }
 
 //Sound List
+function whatSound(whatColor) {
+  switch (whatColor) {
+    case "red":
+      const redColor = new Audio("./sounds/red.mp3");
+      redColor.play();
+      break;
+    case "green":
+      const greenColor = new Audio("./sounds/green.mp3");
+      greenColor.play();
+      break;
+    case "blue":
+      const blueColor = new Audio("./sounds/blue.mp3");
+      blueColor.play();
+      break;
+    case "yellow":
+      const yellowColor = new Audio("./sounds/yellow.mp3");
+      yellowColor.play();
+      break;
+    case "wrong":
+      const wrongSound = new Audio("./sounds/wrong.mp3");
+      wrongSound.play();
+      break;
+    default:
+      break;
+  }
+}
