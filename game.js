b6bf34ab7c1a6a1f5904c1e8dab16d9ae3a2b66b
radityarps <radityarafifps@gmail.com>
2023-07-28 22:12:16 +0700

    complete game system
    1. Game Over system
    2. Check Pattern
    etc.

--- game.js
+++ game.js
@@ -1,17 +1,17 @@
 // Game Path
-const userClickedPattern = [];
-const gamePattern = [];
-//Random Pick Color
-var buttonColors = ["red", "blue", "green", "yellow"];
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
-  $("#" + randomChosenColor)
-    .fadeOut(100)
-    .fadeIn(100);
-  whatSound(randomChosenColor);
-}
+var userClickedPattern = [];
+var gamePattern = [];
+
+//Start The Game
+var started = false;
+var level = 0;
+$(document).on("keydown", function () {
+  if (started === false) {
+    nextSequence();
+    $("#level-title").text("Level " + level);
+    started = true;
+  }
+});
 
 //Clicked Button
 $(".btn").on("click", function () {
@@ -22,7 +22,48 @@ $(".btn").on("click", function () {
     $("#" + userChosenColor).removeClass("pressed");
   }, 200);
   whatSound(userChosenColor);
+  checkPattern(userClickedPattern.length - 1);
 });
+
+//Game system
+function checkPattern(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    const wrongSound = new Audio("./sounds/wrong.mp3");
+    wrongSound.play();
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text(
+      "GameOver at level " + level + ", Press Any Key to Restart"
+    );
+    level = 0;
+    started = false;
+    gamePattern = [];
+    userClickedPattern = [];
+  }
+}
+
+//Random Pick Color
+var buttonColors = ["red", "blue", "green", "yellow"];
+function nextSequence() {
+  userClickedPattern = [];
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100);
+  whatSound(randomChosenColor);
+  level++;
+  $("#level-title").text("Level " + level);
+}
 
 //Sound List
 function whatSound(whatColor) {
@@ -42,10 +83,6 @@ function whatSound(whatColor) {
     case "yellow":
       const yellowColor = new Audio("./sounds/yellow.mp3");
       yellowColor.play();
-      break;
-    case "wrong":
-      const wrongSound = new Audio("./sounds/wrong.mp3");
-      wrongSound.play();
       break;
     default:
       break;
